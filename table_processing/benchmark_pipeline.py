2d296db708dc23751f069a559ebd7702e1fa2143
Ziyad Gaffar <ziyad.gaffar@hc-sc.gc.ca>
2023-07-24 14:30:59 -0400

    Update benchmark_pipeline.py

--- table_processing/benchmark_pipeline.py
+++ table_processing/benchmark_pipeline.py
@@ -2,7 +2,7 @@ from GeneratedTable import GeneratedTable
 from Table_Detector import Table_Detector
 
 
-test_table = GeneratedTable(rows=30, columns=6, vertical_lines=True, row_lines=True)
+test_table = GeneratedTable(rows=30, vertical_lines=True, row_lines=True)
 test_table.to_pdf()
 
 # table_det = Table_Detector(test_table.get_path())
