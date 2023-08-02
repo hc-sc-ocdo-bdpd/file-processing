193e01d0e11540aa9961242a1f88a7eaadf72628
Louis-Boulet <louis.boulet@hc-sc.gc.ca>
2023-08-02 15:19:54 -0400

    extra line chars in cells fix

--- table_processing/benchmark_pipeline.py
+++ table_processing/benchmark_pipeline.py
@@ -15,12 +15,13 @@ failed_tables = []
 # Loop for n # of tables
 for i in range(0,1):
     # Generate, store, and export to pdf true table
-    genr_table = GeneratedTable(rows=5, columns=5, row_lines=True, vertical_lines=True)
+    genr_table = GeneratedTable(rows=5, columns=5, row_lines=True, vertical_lines=True, multi_row=False, row_height=1)
     true_table = genr_table.df
     genr_table.to_pdf()
     t_name = genr_table.get_filename()
     logging.warning('Generated table ' + t_name)
     file_path = 'generated_tables/' + t_name + '/'  + t_name
+    true_table.to_excel(file_path + '_true.xlsx', index = False)
     # Detect from pdf, export to and read from excel processed table
     try:
         detc_table = Table_Detector(file_path+'.pdf')
@@ -32,6 +33,7 @@ for i in range(0,1):
     except IndexError:  # could not detect table from pdf
         read_table = pd.DataFrame()
         failed_tables.append(t_name)
+        logging.error('Could not detect table ' + t_name + ' from pdf')
     tables[t_name] = [true_table, read_table]
 
 # Calculate table extraction performance metrics and format output dfs
@@ -40,7 +42,7 @@ metrics_df.loc[metrics_df.index.isin(failed_tables), list(metrics_df.columns.val
 summary_df = metrics_df.astype(float).describe().fillna(0).apply(lambda s: s.apply('{0:.3f}'.format))
 summary_df.loc[summary_df.index == 'count'] = summary_df.loc[summary_df.index == 'count'].astype(float).astype(int).astype(str)
 summary_df = summary_df.reset_index().rename(columns={'index':''})
-metrics_df = metrics_df.apply(lambda s: s.apply('{0:.3f}'.format)).reset_index().rename(columns={'index':''})
+metrics_df = metrics_df.apply(lambda s: s.apply('{0:.3f}'.format)).reset_index().rename(columns={'index':'ID'})
 logging.warning(pd.DataFrame(np.row_stack((summary_df.columns, summary_df.to_numpy())), 
                 columns=['']*len(summary_df.columns), index=['']*(1+len(summary_df))))
 
