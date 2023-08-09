3860975f28dcf5fef8f4171cc2a59a48e144f8de
mlortie <Mireille.Lortie@hc-sc.gc.ca>
2023-08-09 16:49:00 -0400

    added call to processing function

--- table_processing/GUI.py
+++ table_processing/GUI.py
@@ -1,4 +1,5 @@
 from dash import Dash, dcc, html, Input, Output, callback
+from Table_processor_main import process_pdf
 
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
@@ -6,10 +7,9 @@ app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div(
     [
-        html.I("Try typing in input 1 & 2, and observe how debounce is impacting the callbacks. Press Enter and/or Tab key in Input 2 to cancel the delay"),
+        html.I("To extract tables from a file, please type in the file name and press Enter"),
         html.Br(),
-        dcc.Input(id="input1", type="text", placeholder="", style={'marginRight':'10px'}),
-        dcc.Input(id="input2", type="text", placeholder="", debounce=True),
+        dcc.Input(id="filename", type="text", placeholder="", debounce=True),
         html.Div(id="output"),
     ]
 )
@@ -17,11 +17,14 @@ app.layout = html.Div(
 
 @callback(
     Output("output", "children"),
-    Input("input1", "value"),
-    Input("input2", "value"),
+    Input("filename", "value"),
 )
-def update_output(input1, input2):
-    return f'Input 1 {input1} and Input 2 {input2}'
+def call_processing_fn(filename):
+    if filename != None:
+        output_file = process_pdf(filename)
+        return f'Processing completed.  Extracted tables can be found in {output_file}'
+    else:
+        return f'Standing by for an input file path.'
 
 
 if __name__ == "__main__":
