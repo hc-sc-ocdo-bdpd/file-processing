447fbcac3e46b38684796f4571dba52e99830724
Patrick Gavigan <Patrick.Gavigan@hc-sc.gc.ca>
2023-08-09 09:45:16 -0400

    cleanup

--- table_processing/Table_Processor_Main.py
+++ table_processing/Table_Processor_Main.py
@@ -1,13 +1,4 @@
-# Import table classes, functions, and packages
-
-import subprocess
-import sys
-    
 from Table_Detector import Table_Detector
-from table_metrics import test_tables
-import pandas as pd
-import numpy as np
-# xlsxwriter not imported, but needs to be downloaded in environment
 import logging
 logging.basicConfig(filename='table_detection.log', filemode='a', datefmt='%Y-%m-%d %H:%M:%S',
                     level=logging.INFO, format='[%(asctime)s][%(levelname)s] %(message)s\n')
@@ -17,12 +8,18 @@ file_path = input('Please enter the path to the PDF to extract tables from: ')
 file_path = file_path.replace('\"', '').replace('\\', '\\\\').split('.')[0]
 
 try:
-    detector = Table_Detector(file = file_path+'.pdf')
-    detector.to_excel(filename = file_path+'.xlsx')
+    logging.info("Table Processor started")
+    pdf_input = file_path+'.pdf'
+    detector = Table_Detector(file = pdf_input)
+    logging.info("Processing " + file_path)
+    xlsx_output = file_path+'.xlsx'
+    detector.to_excel(filename = xlsx_output)
+    logging.info("Output saved to " + xlsx_output)
 
 except Exception as e:  # could not detect table from pdf
-    logging.error('Could not detect table ' + file_path + ' from pdf')
+    logging.error('An error occured while processing ' + file_path + '.')
     logging.error(e)
 
-# terminal command to run
+
+# terminal command to build the exe
 # pyinstaller --onefile --hidden-import=pytorch --hidden-import=timm --collect-data timm --collect-data torch --copy-metadata torch --copy-metadata tqdm --copy-metadata regex --copy-metadata requests --copy-metadata packaging --copy-metadata filelock --copy-metadata numpy --copy-metadata tokenizers --copy-metadata huggingface_hub --copy-metadata safetensors --copy-metadata pyyaml --copy-metadata transformers --hidden-import=transformers --collect-all timm --paths ./.venv/Lib/site-packages ./table_processing/Table_Processor_Main.py   
