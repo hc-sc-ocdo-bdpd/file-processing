7dddf25d9f703cb77a584be5ff6c9032034cc89e
Patrick Gavigan <Patrick.Gavigan@hc-sc.gc.ca>
2023-08-09 17:16:42 -0400

    Adding bulletproofing to the gui

--- table_processing/GUI.py
+++ table_processing/GUI.py
@@ -1,5 +1,6 @@
 from dash import Dash, dcc, html, Input, Output, callback
 from Table_processor_main import process_pdf
+from pathlib import Path
 
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
@@ -19,12 +20,14 @@ app.layout = html.Div(
     Output("output", "children"),
     Input("filename", "value"),
 )
-def call_processing_fn(filename):
-    if filename != None:
+def call_processing_fn(filename:str):
+    path = Path(filename)
+    if path.exists() and path.suffix == ".pdf":
         output_file = process_pdf(filename)
         return f'Processing completed.  Extracted tables can be found in {output_file}'
     else:
-        return f'Standing by for an input file path.'
+        return f''
+
 
 
 if __name__ == "__main__":
