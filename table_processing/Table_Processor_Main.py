2af828964d3856a59fc056472c314119d89dcf68
Patrick Gavigan <Patrick.Gavigan@hc-sc.gc.ca>
2023-08-09 13:40:17 -0400

    refactored main into function calls

--- table_processing/Table_Processor_Main.py
+++ table_processing/Table_Processor_Main.py
@@ -5,19 +5,28 @@ logging.basicConfig(filename='table_detection.log', filemode='a', datefmt='%Y-%m
 logging.getLogger().addHandler(logging.StreamHandler())
 
 
-logging.info("Table Processor started")
-file_path = input('Please enter the path to the PDF to extract tables from: ')
-file_path = file_path.replace('\"', '').replace('\\', '\\\\').split('.')[0]
-pdf_input = file_path+'.pdf'
-xlsx_output = file_path+'.xlsx'
-logging.info("Processing " + pdf_input)
+def process_pdf(file_path):
+    logging.info("Processing path provided: " + file_path)
+    file_path = file_path.replace('\"', '').replace('\\', '\\\\').split('.')[0]
+    pdf_input = file_path+'.pdf'
+    xlsx_output = file_path+'.xlsx'
+    logging.info("Processing file: " + pdf_input)
 
-try:
-    detector = Table_Detector(file = pdf_input)
-    logging.info("Saving output to " + xlsx_output)
-    detector.to_excel(filename = xlsx_output)
-except Exception as e:  # could not detect table from pdf
-    logging.error('An error occured while processing ' + file_path + '.')
-    logging.error(e)
+    try:
+        detector = Table_Detector(file = pdf_input)
+        logging.info("Saving output to: " + xlsx_output)
+        detector.to_excel(filename = xlsx_output)
+    except Exception as e:  # could not detect table from pdf
+        logging.error('An error occured while processing: ' + file_path + '.')
+        logging.error(e)
 
-logging.info("Table Processor finished")
+
+def console_main():
+    logging.info("Table Processor started")
+    file_path = input('Please enter the path to the PDF to extract tables from: ')
+    process_pdf(file_path)
+    logging.info("Table Processor finished")
+
+
+if __name__ == '__main__':
+    console_main()
