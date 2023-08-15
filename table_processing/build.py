266c632db91a1a285bb5abdf75e444902f6c04b2
Patrick Gavigan <Patrick.Gavigan@hc-sc.gc.ca>
2023-08-15 09:31:40 -0400

    refactor build tool

--- table_processing/build.py
+++ table_processing/build.py
@@ -5,35 +5,40 @@ logging.basicConfig(filename='build_log.log', filemode='a', datefmt='%Y-%m-%d %H
                     level=logging.INFO, format='[%(asctime)s][%(levelname)s] %(message)s\n')
 logging.getLogger().addHandler(logging.StreamHandler())
 
-logging.info("Building applications.")
-logging.info("Cleaning up previous build files for the console application.")
-console_app_py = './table_processing/Table_Processor_Main.py'
-PyInstaller.__main__.run(['--clean', console_app_py])
-logging.info("Building console application.")
-PyInstaller.__main__.run([
-    console_app_py,
-    '--onefile',
-    '--windowed',
-    '--hidden-import=pytorch',
-    '--hidden-import=timm',
-    '--collect-data timm',
-    '--collect-data torch',
-    '--copy-metadata torch',
-    '--copy-metadata tqdm',
-    '--copy-metadata regex',
-    '--copy-metadata requests',
-    '--copy-metadata packaging',
-    '--copy-metadata filelock',
-    '--copy-metadata numpy',
-    '--copy-metadata tokenizers',
-    '--copy-metadata huggingface_hub',
-    '--copy-metadata safetensors',
-    '--copy-metadata pyyaml',
-    '--copy-metadata transformers',
-    '--hidden-import=transformers'
-    '--collect-all timm',
-    '--paths ./.venv/Lib/site-packages'
-])
-logging.info("Console application build complete.")
+def build_application(console_app_py):
+    logging.info("Building application: " + str(console_app_py) + ".")
+    logging.info("Cleaning up previous build files for the console application.")
+    PyInstaller.__main__.run(['--clean', console_app_py])
+    logging.info("Building console application: " + str(console_app_py) + ".")
+    PyInstaller.__main__.run([
+        console_app_py,
+        '--onefile',
+        '--windowed',
+        '--hidden-import=pytorch',
+        '--hidden-import=timm',
+        '--collect-data timm',
+        '--collect-data torch',
+        '--copy-metadata torch',
+        '--copy-metadata tqdm',
+        '--copy-metadata regex',
+        '--copy-metadata requests',
+        '--copy-metadata packaging',
+        '--copy-metadata filelock',
+        '--copy-metadata numpy',
+        '--copy-metadata tokenizers',
+        '--copy-metadata huggingface_hub',
+        '--copy-metadata safetensors',
+        '--copy-metadata pyyaml',
+        '--copy-metadata transformers',
+        '--hidden-import=transformers'
+        '--collect-all timm',
+        '--paths ./.venv/Lib/site-packages'
+    ])
+    logging.info("Console application build complete: " + str(console_app_py))
 
-# TODO: Build the GUI application
+
+
+if __name__ == '__main__':
+    build_application('./table_processing/Table_Processor_Main.py')
+    build_application('./table_processing/GUI.py')
+    
