e9c7e386e28522177c97640cadec5159db73ad1e
Brennan Cruse <brennan.cruse@hc-sc.gc.ca>
2023-09-01 11:25:57 -0400

    add docx functionality

--- file_processing/docx_processor.py
+++ file_processing/docx_processor.py
@@ -1,3 +1,24 @@
 from file_processor_strategy import FileProcessorStrategy
+from docx import Document
 
-# To implement
+class DocxFileProcessor(FileProcessorStrategy):
+    def __init__(self, file_path):
+        super().__init__(file_path)
+        self.metadata = {}
+
+    def process(self):
+        text = self.extract_text_from_docx(self.file_path)
+        if text is not None:
+            self.metadata.update({'text': text})
+
+    @staticmethod
+    def extract_text_from_docx(file_path):
+        try:
+            doc = Document(file_path)
+            full_text = []
+            for para in doc.paragraphs:
+                full_text.append(para.text)
+            return '\n'.join(full_text)
+        except Exception as e:
+            print(f"Error encountered while opening or processing {file_path}: {e}")
+            return None
