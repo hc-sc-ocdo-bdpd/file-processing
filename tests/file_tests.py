c61b02e670738f7f1efc4869f8ef448cf46e4bbc
Brennan Cruse <brennan.cruse@hc-sc.gc.ca>
2023-09-01 12:32:50 -0400

    Added file test base functions

---
+++ tests/file_tests.py
@@ -0,0 +1,16 @@
+import sys, os
+from file_processing.file import File
+
+def test_txt_text_extraction():
+    # to implement
+    return 1
+
+
+def test_pdf_text_extraction():
+    # to implement
+    return 1
+
+
+def test_docx_text_extraction():
+    # to implement
+    return 1
