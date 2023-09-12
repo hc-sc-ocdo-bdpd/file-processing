5dfdf6163bdc674b70897f4da2f151c82148cfdf
Brennan Cruse <brennan.cruse@hc-sc.gc.ca>
2023-09-12 16:38:07 -0400

    Implement OCR
    - Untested for not yet implemented image files
    - Uses the same logic as parallel Document class (old code) for pdfs

--- file_processing/ocr_decorator.py
+++ file_processing/ocr_decorator.py
@@ -1,3 +1,13 @@
+import PyPDF2
+import pytesseract
+from PIL import Image
+import os
+import io
+import getpass
+
+# Init for tesseract
+pytesseract.pytesseract.tesseract_cmd = os.path.join('C:/Users', getpass.getuser(), 'AppData/Local/Programs/Tesseract-OCR/tesseract.exe')
+
 class OCRDecorator:
     def __init__(self, processor):
         """Initializes the OCRDecorator with a given file processor."""
@@ -7,10 +17,8 @@ class OCRDecorator:
         """Processes the file using the wrapped processor and then applies OCR."""
         self._processor.process()
         ocr_text = self.extract_text_with_ocr()
-        if 'text' in self._processor.metadata:
-            self._processor.metadata['text'] += ocr_text
-        else:
-            self._processor.metadata['text'] = ocr_text
+        self._processor.metadata['ocr_text'] = ocr_text
+
 
     def extract_text_with_ocr(self):
         """Extracts text from the file using OCR.
@@ -18,8 +26,51 @@ class OCRDecorator:
         Returns:
             str: The extracted text.
         """
-        # TODO: Implement OCR extraction logic here
-        pass
+        extension = self._processor.extension
+            
+        if extension == ".pdf":
+            return self._ocr_pdf()
+        else:
+            return self._ocr_image()
+
+    def _ocr_image(self):
+        try:
+            image = Image.open(self._processor.file_path)
+            ocr_result = pytesseract.image_to_string(image)
+            return ocr_result
+        except Exception as e:
+            print(f"Error during OCR processing for image: {e}")
+            return ""
+
+    def _ocr_pdf(self):
+        text = ''
+        ocrText = ''
+
+        try:
+            pdfFileObj = open(self._processor.file_path, 'rb')
+            reader = PyPDF2.PdfReader(pdfFileObj)
+            numPages = len(reader.pages)
+
+            # Assuming you want to read the whole document for the OCRDecorator
+            startPage = 0
+            endPage = numPages
+
+            for i in range(startPage, endPage):
+                pageObj = reader.pages[i]
+                
+                text += pageObj.extract_text()  # extract the text of the page
+                
+                for image in pageObj.images:  # extract the images of the page
+                    ocrText += pytesseract.image_to_string(Image.open(io.BytesIO(image.data)))
+            pdfFileObj.close()
+
+            # Combine the extracted text and OCR text
+            combined_text = text + "\n" + ocrText
+            return combined_text
+
+        except Exception as e:
+            print(f"Error during OCR processing for PDF: {e}")
+            return ""
 
     @property
     def file_name(self):
