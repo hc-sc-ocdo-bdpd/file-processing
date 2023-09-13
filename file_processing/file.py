6d427354c24f4a6e982a4c6782acad8fc946b9b1
Brennan Cruse <brennan.cruse@hc-sc.gc.ca>
2023-09-13 08:57:38 -0400

    Replace series of if's with .get() and dictionary

--- file_processing/file.py
+++ file_processing/file.py
@@ -7,6 +7,12 @@ from ocr_decorator import OCRDecorator
 class File:
     OCR_APPLICABLE_EXTENSIONS = {".pdf", ".jpeg", ".png"}
 
+    PROCESSORS = {
+        ".txt": TextFileProcessor,
+        ".pdf": PdfFileProcessor,
+        ".docx": DocxFileProcessor,
+    }
+
     def __init__(self, path, use_ocr=False):
         self.path = path
         self.processor = self._get_processor(use_ocr)
@@ -14,21 +20,18 @@ class File:
 
     def _get_processor(self, use_ocr):
         _, extension = os.path.splitext(self.path)
-        processor = None
 
-        if extension == ".txt":
-            processor = TextFileProcessor(self.path)
-        elif extension == ".pdf":
-            processor = PdfFileProcessor(self.path)
-        elif extension == ".docx":
-            processor = DocxFileProcessor(self.path)
-        else:
+        processor_class = File.PROCESSORS.get(extension)
+        if not processor_class:
             raise ValueError(f"No processor for file type {extension}")
+
+        processor = processor_class(self.path)
 
         if use_ocr:
             if extension not in File.OCR_APPLICABLE_EXTENSIONS:
                 raise ValueError(f"OCR is not applicable for file type {extension}.")
             return OCRDecorator(processor)
+        
         return processor
 
     def process(self):
