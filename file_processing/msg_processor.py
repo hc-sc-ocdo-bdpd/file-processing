dcca44d81365032a64d550655d197aeb267126f1
taylenb <tburn049@uottawa.ca>
2023-09-18 13:55:43 -0400

    Created msg class

--- file_processing/msg_processor.py
+++ file_processing/msg_processor.py
@@ -1,3 +1,16 @@
 from file_processor_strategy import FileProcessorStrategy
+import extract_msg 
 
-# To implement
+class msgFileProcessor(FileProcessorStrategy):
+    def __init__(self, file_path: str) -> None:
+        super().__init__(file_path)
+        self.metadata = {}
+    
+    def process(self) -> None:
+        msg = extract_msg.Message(self.file_path)
+        self.metadata.update({'text': msg.body})
+        self.metadata.update({'subject': msg.subject})
+        self.metadata.update({'date': msg.date})
+        self.metadata.update({'sender': msg.sender})
+
+
