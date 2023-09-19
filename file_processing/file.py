e0e1d10103313c38091471f029634da85ede5330
Karolina Prazuch <69173333+kprazuch@users.noreply.github.com>
2023-09-19 13:04:46 -0400

    Writing pptx_processor

--- file_processing/file.py
+++ file_processing/file.py
@@ -4,6 +4,7 @@ from pdf_processor import PdfFileProcessor
 from docx_processor import DocxFileProcessor
 from ocr_decorator import OCRDecorator
 from msg_processor import msgFileProcessor
+from pptx_processor import PptxFileProcessor
 
 class File:
     OCR_APPLICABLE_EXTENSIONS = {".pdf", ".jpeg", ".png"}
@@ -13,6 +14,7 @@ class File:
         ".pdf": PdfFileProcessor,
         ".docx": DocxFileProcessor,
         ".msg": msgFileProcessor,
+        ".pptx": PptxFileProcessor
     }
 
     def __init__(self, path: str, use_ocr: bool = False) -> None:
--- file_processing/pptx_processor.py
+++ file_processing/pptx_processor.py
@@ -1,3 +1,29 @@
 from file_processor_strategy import FileProcessorStrategy
+from pptx import Presentation
 
-# To implement
+class PptxFileProcessor(FileProcessorStrategy):
+    def __init__(self, file_path: str) -> None:
+        super().__init__(file_path)
+        self.metadata = {}
+
+    def process(self) -> None:
+        ppt = Presentation(self.file_path)
+        self.metadata.update({'text': self.extract_text_from_pptx(ppt)})
+        self.metadata.update({'author': ppt.core_properties.author})
+        self.metadata.update({'last_modified_by': ppt.core_properties.last_modified_by})
+
+        # Other core properties to include: https://python-docx.readthedocs.io/en/latest/api/document.html#coreproperties-objects
+        # keywords, language, subject, version
+
+    @staticmethod
+    def extract_text_from_pptx(ppt: Presentation) -> str:
+        try:
+            full_text = []
+            for slide in ppt.slides:
+                for shape in slide.shapes:
+                    if hasattr(shape, "text"):
+                        full_text.append(shape.text)
+            return '\n'.join(full_text)
+        except Exception as e:
+            print(f"Error encountered while opening or processing {file_path}: {e}")
+            return None
