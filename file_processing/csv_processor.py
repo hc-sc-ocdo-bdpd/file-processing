f06d61b860ab8327e8c04607cffdb4900d8442f4
Daisy Xiao <122558060+dsxiao@users.noreply.github.com>
2023-10-12 14:28:12 -0400

    Created csv_processor
    In csv_processor.py

--- file_processing/csv_processor.py
+++ file_processing/csv_processor.py
@@ -1,3 +1,38 @@
 from file_processor_strategy import FileProcessorStrategy
+import csv
+import chardet
 
-# To implement
+class CsvFileProcessor(FileProcessorStrategy):
+    def __init__(self, file_path: str) -> None:
+        super().__init__(file_path)
+        self.metadata = {}
+
+    def process(self) -> None:
+        encoding = chardet.detect(open(self.file_path, 'rb').read())['encoding']
+        with open(self.file_path, 'r', encoding=encoding) as f:
+            reader = csv.reader(f)
+            rows = [row for row in reader] # List of rows, e.g. [['row', 'one'], ['row', 'two']]
+            text = '\n'.join([','.join(row) for row in rows])
+            num_cells = 0
+            empty_cells = 0
+            for row in rows:
+                for i in range(len(row)):
+                    num_cells += 1
+                    if row[i] == '':
+                        empty_cells += 1
+            self.metadata.update({
+                'text': text,
+                'encoding': encoding,
+                'num_rows': len(rows),
+                'num_cols': len(rows[0]) if rows else 0,
+                'num_cells': num_cells,
+                'empty_cells': empty_cells
+            })
+    
+    def save(self, output_path: str = None) -> None:
+        save_path = output_path or self.file_path
+        with open(save_path, 'w', encoding = self.metadata['encoding']) as f:
+            writer = csv.writer(f)
+            rows = self.metadata['text'].split('\n')
+            for row in rows:
+                writer.writerow(row.split(','))
