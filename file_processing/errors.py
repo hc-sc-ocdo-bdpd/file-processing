680014ac62c0358775b2c0994ac6d0c0aacb6f6d
Brennan Cruse <brennan.cruse@hc-sc.gc.ca>
2023-10-18 15:55:00 -0400

    Update errors.py

--- file_processing/errors.py
+++ file_processing/errors.py
@@ -1,17 +1,19 @@
 
+
 class FileProcessorError(Exception):
     """Base exception for the file processing library."""
     pass
+
 
 
 class UnsupportedFileTypeError(FileProcessorError):
     """Raised when an unsupported file type is encountered."""
     pass
 
-
 class FileProcessingFailedError(FileProcessorError):
     """Raised when there's a generic error during file processing."""
     pass
+
 
 
 class OCRError(FileProcessorError):
