01278fc092d67daf235a94083ff2f62a14604de3
Brennan Cruse <brennan.cruse@hc-sc.gc.ca>
2023-10-18 15:51:35 -0400

    xml

--- file_processing/xml_processor.py
+++ file_processing/xml_processor.py
@@ -1,5 +1,6 @@
 from file_processor_strategy import FileProcessorStrategy
 import chardet
+from errors import FileProcessingFailedError
 
 class XmlFileProcessor(FileProcessorStrategy):
     def __init__(self, file_path: str) -> None:
@@ -7,23 +8,29 @@ class XmlFileProcessor(FileProcessorStrategy):
         self.metadata = {}
 
     def process(self) -> None:
-        # Reads the file and updates the metadata with information about the file
-        encoding = chardet.detect(open(self.file_path, "rb").read())['encoding']
-        with open(self.file_path, 'r', encoding=encoding) as f:
-            text = f.read()
-            lines = text.split('\n')
-            words = text.split()
+        try:
+            encoding = chardet.detect(open(self.file_path, "rb").read())['encoding']
+            with open(self.file_path, 'r', encoding=encoding) as f:
+                text = f.read()
+                lines = text.split('\n')
+                words = text.split()
 
-        self.metadata.update({
-            'text': text,
-            'encoding': encoding,
-            'lines': lines,
-            'words': words,
-            'num_lines': len(lines),
-            'num_words': len(words),
-        })
+            self.metadata.update({
+                'text': text,
+                'encoding': encoding,
+                'lines': lines,
+                'words': words,
+                'num_lines': len(lines),
+                'num_words': len(words),
+            })
+        except Exception as e:
+            raise FileProcessingFailedError(f"Error encountered while processing {self.file_path}: {e}")
+
         
     def save(self, output_path: str = None) -> None:
-        save_path = output_path or self.file_path
-        with open(save_path, 'w', encoding = self.metadata['encoding']) as f:
-            f.write(self.metadata['text'])
+        try:
+            save_path = output_path or self.file_path
+            with open(save_path, 'w', encoding = self.metadata['encoding']) as f:
+                f.write(self.metadata['text'])
+        except Exception as e:
+            raise FileProcessingFailedError(f"Error encountered while saving {self.file_path}: {e}")
