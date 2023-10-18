51af376ad68d8a592bcba05bd74c0b2c8f81fd02
Brennan Cruse <brennan.cruse@hc-sc.gc.ca>
2023-10-18 15:21:02 -0400

    html

--- file_processing/html_processor.py
+++ file_processing/html_processor.py
@@ -10,23 +10,29 @@ class HtmlFileProcessor(FileProcessorStrategy):
         self.metadata = {}
 
     def process(self) -> None:
-        # Reads the file and updates the metadata with information about the file
-        encoding = chardet.detect(open(self.file_path, "rb").read())['encoding']
-        with open(self.file_path, 'r', encoding=encoding) as f:
-            text = f.read()
-            lines = text.split('\n')
-            words = text.split()
+        try:
+            encoding = chardet.detect(open(self.file_path, "rb").read())['encoding']
+            with open(self.file_path, 'r', encoding=encoding) as f:
+                text = f.read()
+                lines = text.split('\n')
+                words = text.split()
 
-        self.metadata.update({
-            'text': text,
-            'encoding': encoding,
-            'lines': lines,
-            'words': words,
-            'num_lines': len(lines),
-            'num_words': len(words),
-        })
+            self.metadata.update({
+                'text': text,
+                'encoding': encoding,
+                'lines': lines,
+                'words': words,
+                'num_lines': len(lines),
+                'num_words': len(words),
+            })
+        except Exception as e:
+            raise FileProcessingFailedError(f"Error encountered while processing: {e}")
+
 
     def save(self, output_path: str = None) -> None:
-        save_path = output_path or self.file_path
-        with open(save_path, 'w', encoding = self.metadata['encoding']) as f:
-            f.write(self.metadata['text'])
+        try:
+            save_path = output_path or self.file_path
+            with open(save_path, 'w', encoding = self.metadata['encoding']) as f:
+                f.write(self.metadata['text'])
+        except Exception as e:
+            raise FileProcessingFailedError(f"Error encountered while saving: {e}")
