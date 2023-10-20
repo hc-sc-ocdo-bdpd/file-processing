c5997079ab19682e6d0364e569f8aa19cfe16537
Patrick Gavigan <Patrick.Gavigan@hc-sc.gc.ca>
2023-10-20 15:07:36 -0400

    cleaned up formatting

--- file_processing/errors.py
+++ file_processing/errors.py
@@ -5,28 +5,30 @@ class FileProcessorError(Exception):
     pass
 
 
-
 class UnsupportedFileTypeError(FileProcessorError):
     """Raised when an unsupported file type is encountered."""
     pass
 
+
 class FileProcessingFailedError(FileProcessorError):
     """Raised when there's a generic error during file processing."""
     pass
+
 
 class FileCorruptionError(FileProcessorError):
     """Raised when the file is found to be corrupted."""
     pass
 
 
-
 class OCRError(FileProcessorError):
     """Base exception for OCR related issues."""
     pass
 
+
 class OCRProcessingError(OCRError):
     """Raised when there's an issue during OCR processing."""
     pass
+
 
 class NotOCRApplciableError(OCRError):
     """Raised when attempting OCR on a file type that doesn't support it."""
