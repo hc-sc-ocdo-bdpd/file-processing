04e5460c00374d4b2f71091c1d10bdc3838e326b
Karolina Prazuch <69173333+kprazuch@users.noreply.github.com>
2023-10-30 16:55:54 -0400

    xlsx test refactoring

--- tests/test_xlsx_processor.py
+++ tests/test_xlsx_processor.py
@@ -3,4 +3,54 @@ import sys, os
 sys.path.append(os.path.join(sys.path[0],'file_processing'))
 from file_processing.file import File
 
-# To implement
+variable_names = "path, sheet_names, active_sheet, data, last_modified_by, creator"
+values = [
+   ('tests/resources/test_files/Test_excel_file.xlsx', ['Sheet1', 'Sheet2', 'Sheet3'], "Sheet3", 42, 'Burnett, Taylen (HC/SC)', 'Burnett, Taylen (HC/SC)')
+]
+
+
+@pytest.mark.parametrize(variable_names, values)
+def test_xlsx_metadata(path, sheet_names, active_sheet, data, last_modified_by, creator):
+    file_obj = File(path)
+    assert file_obj.metadata['sheet_names'] == sheet_names
+    assert file_obj.metadata['active_sheet'] == active_sheet
+    assert sum(len(file_obj.metadata["data"][x]) for x in file_obj.metadata["data"].keys()) == data
+    assert file_obj.metadata['last_modified_by'] == last_modified_by
+    assert file_obj.metadata['creator'] == creator
+
+
+locked_files = [
+     ('tests/resources/test_files/Test_excel_file_Locked.xlsx'), 
+     ('tests/resources/test_files/StructureofCanadianFederalGovFromWikipedia_Locked.xlsx')
+]
+
+@pytest.mark.parametrize("path", locked_files)
+def test_xlsx_locked(path):
+    assert File(path).metadata["has_password"] == True
+
+
+@pytest.mark.parametrize("path, sheet_names, active_sheet, data", map(lambda x: x[:4], values))
+def test_save_xlsx_metadata(copy_file, sheet_names, active_sheet, data):
+        
+        # Load and change metadata via File object
+        xlsx_file = File(copy_file)
+        xlsx_file.metadata['last_modified_by'] = 'Modified Creator'
+        xlsx_file.metadata['creator'] = 'New Creator'
+
+        # Save the updated file
+        xlsx_file.save()
+        test_xlsx_metadata(copy_file, sheet_names, active_sheet, data, 'Modified Creator', 'New Creator')
+
+
+@pytest.mark.parametrize("path", map(lambda x: x[0], values))
+def test_xlsx_invalid_save_location(invalid_save_location):
+    invalid_save_location
+
+
+corrupted_files = [
+    'tests/resources/test_files/Test_excel_file_corrupted.xlsx'
+]
+
+@pytest.mark.parametrize("path", corrupted_files)
+def test_xlsx_corrupted_file_processing(corrupted_file_processing_lock):
+    corrupted_file_processing_lock
