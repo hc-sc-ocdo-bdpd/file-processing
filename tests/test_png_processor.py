233d8fd98efd441b481d8c58298773140884dfe0
taylenb <tburn049@uottawa.ca>
2023-10-30 14:32:41 -0400

    Updating test_png_processor

--- tests/test_png_processor.py
+++ tests/test_png_processor.py
@@ -3,8 +3,9 @@ import sys, os
 sys.path.append(os.path.join(sys.path[0],'file_processing'))
 from file_processing.file import File
 
-# To do: fix test_save_png_metadata
-# Refactor invalid_save_location and _corrupted_file_processing
+# Note: test_save_png_metadata tests both files for "original_format == 'PNG'
+# when creating a copy, the original_format metadata for Health_Canada_logo.png
+# becomes 'GIF' --> 'PNG'
 
 variable_names = "path, original_format, mode, width, height"
 values = [
@@ -26,21 +27,18 @@ def test_png_metadata(path, original_format, mode, width, height):
 def test_save_png_metadata(copy_file, original_format, mode, width, height):
         png = File(copy_file)
         png.save()
-        test_png_metadata(copy_file, original_format, mode, width, height)
+        test_png_metadata(copy_file, 'PNG', mode, width, height)
 
 
-def test_png_invalid_save_location():
-    import pytest
-    from file_processing.file import File
-    from errors import FileProcessingFailedError
-    
-    png_file = File('tests/resources/test_files/Health_Canada_logo.png')
-    with pytest.raises(FileProcessingFailedError):
-        png_file.processor.save('/non_existent_folder/Health_Canada_logo.png')
+@pytest.mark.parametrize("path", map(lambda x: x[0], values))
+def test_docx_invalid_save_location(invalid_save_location):
+    invalid_save_location
 
-def test_png_corrupted_file_processing():
-    import pytest
-    from file_processing.file import File
-    from errors import FileProcessingFailedError
-    with pytest.raises(FileProcessingFailedError) as exc_info:
-        File("tests/resources/test_files/MapCanada_corrupted.png")
+corrupted_files = [
+    'tests/resources/test_files/HealthCanadaOverviewFromWikipedia_corrupted.pptx'
+]
+
+@pytest.mark.parametrize("path", corrupted_files)
+def test_docx_corrupted_file_processing(corrupted_file_processing_lock):
+    corrupted_file_processing_lock
+
