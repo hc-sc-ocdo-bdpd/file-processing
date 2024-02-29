efe4ae6b26c261a097689e94687da16220f9da24
benjaminLuoHC <155466242+benjaminLuoHC@users.noreply.github.com>
2024-02-29 14:03:15 -0500

    Integrated batch runner

--- file_processing/directory.py
+++ file_processing/directory.py
@@ -19,32 +19,48 @@ class Directory:
         self.path = path
         self.use_ocr = use_ocr
 
-    def _file_generator(self, filters: dict = None, open_files: bool = True):
+    def _file_generator(self, filters: dict = None, open_files: bool = True,
+                        start_at: int = 0, batch_size: int = 0):
         filters = filters or {}
+        batch = []
 
-        total_files = sum(len(files) for _, _, files in os.walk(self.path))
-
-        with tqdm(total=total_files, desc='Processing files', unit='file') as pbar:
-            for root, _, filenames in os.walk(self.path):
+        with tqdm(desc='Processing files', unit=' files completed') as pbar:
+            for dirpath, _, filenames in os.walk(self.path):
                 for filename in filenames:
-                    file_path = os.path.join(root, filename)
-
                     pbar.update(1)
+                    file_path = os.path.join(dirpath, filename)
 
                     if filters and not self._apply_filters(file_path, filters):
                         continue
 
-                    try:
-                        file = File(file_path, use_ocr=self.use_ocr, open_file=open_files)
-                        logging.info('Processing file: %s', file_path)
-                        yield file
-                    except Exception as e:
-                        logging.error('Error processing %s: %s', file_path, type(e).__name__)
-                        file = File(file_path, open_file=False)
-                        file.metadata.pop('message')
-                        file.metadata.update({'error': type(e).__name__})
+                    # Start processing when the starting index is reached
+                    if pbar.n > start_at:
+                        file_obj = None
 
-                        yield file
+                        # Process the file
+                        try:
+                            file_obj = File(file_path, use_ocr=self.use_ocr, open_file=open_files)
+                            logging.info('Processing file: %s', file_path)
+                        except Exception as e:
+                            logging.error('Error processing %s: %s', file_path, type(e).__name__)
+                            file_obj = File(file_path, open_file=False)
+                            file_obj.metadata.pop('message')
+                            file_obj.metadata.update({'error': type(e).__name__})
+
+                        # Batching; yield if batch_size = 0, otherwise wait until the batch size is reached
+                        if batch_size == 0:
+                            yield file_obj
+                        elif batch_size > 0:
+                            batch.append(file_obj)
+                            batch_progress = pbar.n/batch_size
+
+                            if int(batch_progress) == batch_progress:
+                                yield batch
+                                batch = []
+
+            # Yield the last batch
+            if len(batch) > 0:
+                yield batch
 
     def _apply_filters(self, file_path: str, filters: dict) -> bool:
         file = Path(file_path).resolve()
@@ -172,7 +188,7 @@ class Directory:
     def generate_report(self, report_file: str, include_text: bool = False, filters: Optional[dict] = None,
                         keywords: Optional[list] = None, migrate_filters: Optional[dict] = None,
                         open_files: bool = True, split_metadata: bool = False, char_limit: int = 3000,
-                        batch_size: int = 10000) -> None:
+                        batch_size: int = 500, start_at: int = 0, recovery_mode: bool = False) -> None:
         """
         Generates a report of the directory and writes it to a CSV file.
 
@@ -185,62 +201,78 @@ class Directory:
         :param split_metadata: Whether to unpack the metadata dictionary into separate columns in the CSV file.
         :param char_limit: The cut-off length for each metadata field.
         :param batch_size: The maximum number of files per report. If exceeded, then additional reports are created.
+        :param start_at: Start analyzing files starting at this index.
+        :param recovery_mode: Continue building on the output report instead of rebuilding it.
         """
 
-        # Extracting the attributes from the File object
-        data = [file.processor.__dict__ for file in self._file_generator(filters, open_files)]
+        # Crash recovery: auto-computing start_at index to see when to start processing
+        if recovery_mode and os.path.isfile(report_file):
+            df = pd.read_csv(report_file)
+            start_at = len(df)
 
-        # Imposing a character limit on each metadata property, or removing the verbose fields
-        for file in data:
-            file.pop('open_file', None)
-            file['size'] = file['size'] / 1e6
-            if migrate_filters:
-                file['migrate'] = int(self._apply_filters(file['file_path'], migrate_filters))
-            if include_text and open_files:
-                file['metadata'] = {k: str(v)[:char_limit] for k, v in file['metadata'].items()}
-                if keywords and file['metadata'].get('text'):
-                    file['keywords'] = self._count_keywords(file['metadata']['text'], keywords)
-                elif keywords:
-                    file['keywords'] = self._count_keywords('', keywords)
-            elif not include_text:
-                for field in ['text', 'docstrings', 'imports', 'words', 'lines', 'data']:
-                    file['metadata'].pop(field, None)
+        with tqdm(desc='Processing batches', unit=' batches completed') as pbar:
+            for index, batch in enumerate(self._file_generator(filters, open_files, start_at, batch_size)):
+                # Extracting the attributes from the File object
+                data = [file.processor.__dict__ for file in batch]
 
-        # Unpacking the metadata field so each metadata property becomes its own column
-        if split_metadata:
-            data = pd.json_normalize(data, max_level=1, sep='_')
+                # Imposing a character limit on each metadata property, or removing the verbose fields
+                for file in data:
+                    file.pop('open_file', None)
+                    file['size'] = file['size'] / 1e6
+                    if migrate_filters:
+                        file['migrate'] = int(self._apply_filters(file['file_path'], migrate_filters))
+                    if include_text and open_files:
+                        file['metadata'] = {k: str(v)[:char_limit] for k, v in file['metadata'].items()}
+                        if keywords and file['metadata'].get('text'):
+                            file['keywords'] = self._count_keywords(file['metadata']['text'], keywords)
+                        elif keywords:
+                            file['keywords'] = self._count_keywords('', keywords)
+                    elif not include_text:
+                        for field in ['text', 'docstrings', 'imports', 'words', 'lines', 'data']:
+                            file['metadata'].pop(field, None)
 
-        df = pd.DataFrame(data)
+                # Unpacking the metadata field so each metadata property becomes its own column
+                if split_metadata:
+                    data = pd.json_normalize(data, max_level=1, sep='_')
 
-        if df.empty:
-            raise EmptySelection(f'Filtered selection of files is empty. \
-                            Please try a different directory, or new filters. \
-                            Filters: {filters}, Path: {Path(self.path).resolve()}')
+                df = pd.DataFrame(data)
 
-        df.columns = df.columns.str.replace('metadata_', '')
-        df.columns = df.columns.str.replace('keywords_', 'Keyword.')
+                if df.empty:
+                    raise EmptySelection(f'Filtered selection of files is empty. \
+                                    Please try a different directory, or new filters. \
+                                    Filters: {filters}, Path: {Path(self.path).resolve()}')
 
-        if not split_metadata:
-            df['metadata'] = df['metadata'].apply(json.dumps)
+                df.columns = df.columns.str.replace('metadata_', '')
+                df.columns = df.columns.str.replace('keywords_', 'Keyword.')
 
-        # Converting booleans to integers (True->1; False->0)
-        for boolean in ['is_file', 'is_symlink']:
-            df[boolean] = df[boolean].astype(int)
+                if not split_metadata:
+                    df['metadata'] = df['metadata'].apply(json.dumps)
 
-        # Converting unix time to datetime (GMT time)
-        for time in ['modification_time', 'access_time', 'creation_time']:
-            df[time] = pd.to_datetime(df[time].round(0), unit='s')
+                # Converting booleans to integers (True->1; False->0)
+                for boolean in ['is_file', 'is_symlink']:
+                    df[boolean] = df[boolean].astype(int)
 
-        # Converting file permissions from number to string (ex '666' to 'Unrestricted')
-        df['permissions'] = df['permissions'].apply(
-            lambda x: f'Unrestricted ({x})' if x in ['666', '777'] else f'Restricted ({x})')
+                # Converting unix time to datetime (GMT time)
+                for time in ['modification_time', 'access_time', 'creation_time']:
+                    df[time] = pd.to_datetime(df[time].round(0), unit='s')
 
-        # df.replace(np.NaN, 'N/A', inplace=True)
-        df.columns = df.columns.str.replace('_', ' ')
-        df.columns = df.columns.str.title()
-        df.rename(columns={'Size': 'Size (MB)'}, inplace=True)
+                # Converting file permissions from number to string (ex '666' to 'Unrestricted')
+                df['permissions'] = df['permissions'].apply(
+                    lambda x: f'Unrestricted ({x})' if x in ['666', '777'] else f'Restricted ({x})')
 
-        df.to_csv(report_file)
+                # df.replace(np.NaN, 'N/A', inplace=True)
+                df.columns = df.columns.str.replace('_', ' ')
+                df.columns = df.columns.str.title()
+                df.rename(columns={'Size': 'Size (MB)'}, inplace=True)
+
+                if index > 1:
+                    report = pd.read_csv(report_file)
+                    report = pd.concat([report, df], ignore_index=True)
+                    report.to_csv(report_file, index=False)
+                elif not recovery_mode:
+                    df.to_csv(report_file, index=False)
+
+                pbar.update(1)
 
     def _count_keywords(self, text: str, keywords: list) -> dict:
         """
