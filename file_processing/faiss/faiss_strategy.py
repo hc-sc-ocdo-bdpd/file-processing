825ce6b0f5cb500410f55bb1a1fe6054dc8b1486
TylerWilsonHC-SC <tyler.wilson@hc-sc.gc.ca>
2024-07-12 09:46:13 -0700

    Created save and load methods for the FAISS index class

--- file_processing/faiss/faiss_strategy.py
+++ file_processing/faiss/faiss_strategy.py
@@ -1,10 +1,25 @@
+import faiss
 import numpy as np
 from abc import ABC, abstractmethod
 
-class FAISSStrategy(ABC):
-    def __init__(self, embeddings: np.ndarray) -> None:
-        self.dimension = embeddings.shape[1]
-        self.index = None
+class FAISSIndex(ABC):
+    def __init__(self, embeddings: np.ndarray = None, file_path: str = None) -> None:
+        self.file_path = file_path
+        if embeddings is None:
+            self.read_index()
+        else:
+            self.dimension = embeddings.shape[1]
+            self.index = None
+
+    def save_index(self, output_path: str = None):
+        if not output_path:
+            output_path = self.file_path
+        faiss.write_index(self.index, output_path)
+
+    def read_index(self, input_path: str = None):
+        if not input_path:
+            input_path = self.file_path
+        self.index = faiss.read_index(input_path)
 
     @abstractmethod
     def query(self):
--- file_processing/faiss/flat_index.py
+++ file_processing/faiss/flat_index.py
@@ -1,13 +1,16 @@
 import faiss
 import numpy as np
-from file_processing.faiss.faiss_strategy import FAISSStrategy
+from file_processing.faiss import faiss_strategy
+from importlib import reload
+reload(faiss_strategy)
 
-class FlatIndex(FAISSStrategy):
-    def __init__(self, embeddings: np.ndarray) -> None:
-        super().__init__(embeddings)
-        index = faiss.IndexFlatL2(self.dimension)
-        index.add(embeddings)
-        self.index = index
+class FlatIndex(faiss_strategy.FAISSIndex):
+    def __init__(self, embeddings: np.ndarray = None, file_path: str = None) -> None:
+        super().__init__(embeddings, file_path)
+        if embeddings is not None:
+            index = faiss.IndexFlatL2(self.dimension)
+            index.add(embeddings)
+            self.index = index
 
     def query(self, xq: np.ndarray, k: int = 1):
         return self.index.search(xq, k)
