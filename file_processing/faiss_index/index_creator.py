a0bf05e76ce0fb6428c82df8d0fee23a99ea4c98
TylerWilsonHC-SC <tyler.wilson@hc-sc.gc.ca>
2024-07-17 13:32:55 -0700

    added docstrings to index_creator

--- file_processing/faiss_index/index_creator.py
+++ file_processing/faiss_index/index_creator.py
@@ -7,6 +7,15 @@ from file_processing.faiss_index import general_index
 
 
 def load_index(file_path: str):
+    """
+    Load a FAISS index from a specified file path.
+
+    Args:
+        file_path (str): The path to the file containing the saved FAISS index.
+
+    Returns:
+        An instance of the corresponding index class.
+    """
     INDEXES = {
         faiss.swigfaiss.IndexFlat: flat_index.FlatIndex,
         faiss.swigfaiss.IndexIVFFlat: IVF_flat_index.IVFFlatIndex,
@@ -21,6 +30,16 @@ def load_index(file_path: str):
 
 
 def create_flat_index(embeddings: np.ndarray, file_path: str = None):
+    """
+    Create a Flat Index using the provided embeddings.
+
+    Args:
+        embeddings (np.ndarray): An array of embeddings to index.
+        file_path (str, optional): The path to save the index. If None, the index is not saved.
+
+    Returns:
+        FlatIndex: An instance of the FlatIndex containing the embeddings.
+    """
     index = flat_index.FlatIndex(embeddings)
     if file_path is not None:
         index.save_index(file_path)
@@ -29,6 +48,17 @@ def create_flat_index(embeddings: np.ndarray, file_path: str = None):
 
 def create_IVF_flat_index(embeddings: np.ndarray, nlist: int = None,
                           file_path: str = None):
+    """
+    Create an IVF Flat Index using the provided embeddings and parameters.
+
+    Args:
+        embeddings (np.ndarray): An array of embeddings to index.
+        nlist (int, optional): Number of clusters for the IVF index. If None, a default will be used.
+        file_path (str, optional): The path to save the index. If None, the index is not saved.
+
+    Returns:
+        IVFFlatIndex: An instance of the IVFFlatIndex containing the embeddings.
+    """
     index = IVF_flat_index.IVFFlatIndex(embeddings, nlist)
     if file_path is not None:
         index.save_index(file_path)
@@ -37,6 +67,18 @@ def create_IVF_flat_index(embeddings: np.ndarray, nlist: int = None,
 
 def create_HNSW_index(embeddings: np.ndarray, M: int = 64,
                       efConstruction: int = 64, file_path: str = None):
+    """
+    Create an HNSW Index using the provided embeddings and parameters.
+
+    Args:
+        embeddings (np.ndarray): An array of embeddings to index.
+        M (int, optional): Number of bi-directional links created for each new element. Default is 64.
+        efConstruction (int, optional): Size of the dynamic list for the nearest neighbors during construction. Default is 64.
+        file_path (str, optional): The path to save the index. If None, the index is not saved.
+
+    Returns:
+        HNSWIndex: An instance of the HNSWIndex containing the embeddings.
+    """
     index = HNSW_index.HNSWIndex(embeddings, M, efConstruction)
     if file_path is not None:
         index.save_index(file_path)
