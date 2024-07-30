1f0051e845884717510bc103bdcdb6c520955805
TylerWilsonHC-SC <tyler.wilson@hc-sc.gc.ca>
2024-07-30 14:19:13 -0700

    added SearchDirectory to file_processing init

--- file_processing/__init__.py
+++ file_processing/__init__.py
@@ -2,5 +2,6 @@ from .file import File
 from .directory import Directory
 from .similarity import CosineSimilarity
 from .similarity import LevenshteinDistance
+from .search_directory import SearchDirectory
 
-__all__ = ['File', 'Directory', 'CosineSimilarity', 'LevenshteinDistance']
+__all__ = ['File', 'Directory', 'SearchDirectory', 'CosineSimilarity', 'LevenshteinDistance']
