a673e7eaec879ff62e11af458c5f68c20c2179af
Brennan Cruse <brennan.cruse@hc-sc.gc.ca>
2024-10-10 09:23:37 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,22 @@ from setuptools import setup, find_packages
 
 def parse_requirements(filename):
     with open(filename, 'r') as file:
-        return file.read().splitlines()
+        lines = file.read().splitlines()
+        # Exclude Git-based dependencies from 'install_requires'
+        requirements = [line for line in lines if not line.startswith("git+")]
+        return requirements
+
+def parse_dependency_links(filename):
+    with open(filename, 'r') as file:
+        lines = file.read().splitlines()
+        # Only include Git-based dependencies in 'dependency_links'
+        dependency_links = [line for line in lines if line.startswith("git+")]
+        return dependency_links
 
 setup(
     name="file-processing",
     version="1.0.0",
     packages=find_packages(),
-    # install_requires=parse_requirements('requirements.txt'),
+    install_requires=parse_requirements('requirements.txt'),
+    dependency_links=parse_dependency_links('requirements.txt'),
 )
