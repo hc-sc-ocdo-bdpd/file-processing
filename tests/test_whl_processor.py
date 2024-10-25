f87d617f4a1ad508ee617dc89e615d28f10427d0
He <brandon.he@hc-sc.gc.ca>
2024-10-25 14:15:52 -0400

    Updated expected test values

--- tests/test_whl_processor.py
+++ tests/test_whl_processor.py
@@ -10,9 +10,9 @@ from file_processing_test_data import get_test_files_path
 
 test_files_path = get_test_files_path()
 
-variable_names = "path, package_name, version, python_compatibility, platform_compatibility, optional_dependencies, non_optional_dependencies, build_tag, author"
+variable_names = "path, package_name, version, python_compatibility, platform_compatibility, non_optional_dependencies, optional_dependencies, author, build_tag"
 values = [
-    (test_files_path / 'numpy-2.1.2-pp310-pypy310_pp73-win_amd64.whl', "numpy", "2.1.2", ">=3.10", 
+    (test_files_path / 'numpy-2.1.2-pp310-pypy310_pp73-win_amd64.whl', "numpy", "2.1.2", ">=3.10", "OS Independent"
      ['numpy>=1.22.4; python_version < "3.11"', 'numpy>=1.23.2; python_version == "3.11"', 'numpy>=1.26.0; python_version >= "3.12"', 'python-dateutil>=2.8.2', 'pytz>=2020.1', 'tzdata>=2022.7'], 
      ['hypothesis>=6.46.1 (extra: test)', 'pytest>=7.3.2 (extra: test)', 'pytest-xdist>=2.2.0 (extra: test)', 'pyarrow>=10.0.1 (extra: pyarrow)', 'bottleneck>=1.3.6 (extra: performance)', 'numba>=0.56.4 (extra: performance)', 'numexpr>=2.8.4 (extra: performance)', 'scipy>=1.10.0 (extra: computation)', 'xarray>=2022.12.0 (extra: computation)', 'fsspec>=2022.11.0 (extra: fss)', 's3fs>=2022.11.0 (extra: aws)', 'gcsfs>=2022.11.0 (extra: gcp)', 
       'pandas-gbq>=0.19.0 (extra: gcp)', 'odfpy>=1.4.1 (extra: excel)', 'openpyxl>=3.1.0 (extra: excel)', 'python-calamine>=0.1.7 (extra: excel)', 'pyxlsb>=1.0.10 (extra: excel)', 'xlrd>=2.0.1 (extra: excel)', 'xlsxwriter>=3.0.5 (extra: excel)', 
@@ -24,28 +24,88 @@ values = [
         'openpyxl>=3.1.0 (extra: all)', 'pandas-gbq>=0.19.0 (extra: all)', 'psycopg2>=2.9.6 (extra: all)', 'pyarrow>=10.0.1 (extra: all)', 'pymysql>=1.0.2 (extra: all)', 'PyQt5>=5.15.9 (extra: all)', 'pyreadstat>=1.2.0 (extra: all)', 'pytest>=7.3.2 (extra: all)', 'pytest-xdist>=2.2.0 (extra: all)', 'python-calamine>=0.1.7 (extra: all)', 
         'pyxlsb>=1.0.10 (extra: all)', 'qtpy>=2.3.0 (extra: all)', 'scipy>=1.10.0 (extra: all)', 's3fs>=2022.11.0 (extra: all)', 'SQLAlchemy>=2.0.0 (extra: all)', 'tables>=3.8.0 (extra: all)', 'tabulate>=0.9.0 (extra: all)', 'xarray>=2022.12.0 (extra: all)', 'xlrd>=2.0.1 (extra: all)', 'xlsxwriter>=3.0.5 (extra: all)', 'zstandard>=0.19.0 (extra: all)']
      "Travis E. Oliphant et al.", "1"),
-    (test_files_path / 'pandas-2.2.3-cp313-cp313t-musllinux_1_2_x86_64.whl', "pandas", "2.2.3", ">=3.7", "manylinux1_x86_64", "Another Author")
+    (test_files_path / 'pandas-2.2.3-cp313-cp313t-musllinux_1_2_x86_64.whl', "pandas", "2.2.3", ">=3.9", "OS Independent", ['numpy>=1.22.4 (python_version < "3.11")',
+    'numpy>=1.23.2 (python_version == "3.11")', 'numpy>=1.26.0 (python_version >= "3.12")', 'python-dateutil>=2.8.2', 'pytz>=2020.1', 'tzdata>=2022.7'], 
+      ['hypothesis>=6.46.1 (extra: test)', 'pytest>=7.3.2 (extra: test)', 'pytest-xdist>=2.2.0 (extra: test)', 'pyarrow>=10.0.1 (extra: pyarrow)', 'bottleneck>=1.3.6 (extra: performance)', 'numba>=0.56.4 (extra: performance)',
+    'numexpr>=2.8.4 (extra: performance)',
+    'scipy>=1.10.0 (extra: computation)',
+    'xarray>=2022.12.0 (extra: computation)',
+    'fsspec>=2022.11.0 (extra: fss)',
+    's3fs>=2022.11.0 (extra: aws)',
+    'gcsfs>=2022.11.0 (extra: gcp)',
+    'pandas-gbq>=0.19.0 (extra: gcp)',
+    'odfpy>=1.4.1 (extra: excel)',
+    'openpyxl>=3.1.0 (extra: excel)',
+    'python-calamine>=0.1.7 (extra: excel)',
+    'pyxlsb>=1.0.10 (extra: excel)',
+    'xlrd>=2.0.1 (extra: excel)',
+    'xlsxwriter>=3.0.5 (extra: excel)',
+    'pyarrow>=10.0.1 (extra: parquet)',
+    'pyarrow>=10.0.1 (extra: feather)',
+    'tables>=3.8.0 (extra: hdf5)',
+    'pyreadstat>=1.2.0 (extra: spss)',
+    'SQLAlchemy>=2.0.0 (extra: postgresql)',
+    'psycopg2>=2.9.6 (extra: postgresql)',
+    'adbc-driver-postgresql>=0.8.0 (extra: postgresql)',
+    'SQLAlchemy>=2.0.0 (extra: mysql)',
+    'pymysql>=1.0.2 (extra: mysql)',
+    'SQLAlchemy>=2.0.0 (extra: sql-other)',
+    'adbc-driver-postgresql>=0.8.0 (extra: sql-other)',
+    'adbc-driver-sqlite>=0.8.0 (extra: sql-other)',
+    'beautifulsoup4>=4.11.2 (extra: html)',
+    'html5lib>=1.1 (extra: html)',
+    'lxml>=4.9.2 (extra: html)',
+    'lxml>=4.9.2 (extra: xml)',
+    'matplotlib>=3.6.3 (extra: plot)',
+    'jinja2>=3.1.2 (extra: output-formatting)',
+    'tabulate>=0.9.0 (extra: output-formatting)',
+    'PyQt5>=5.15.9 (extra: clipboard)',
+    'qtpy>=2.3.0 (extra: clipboard)',
+    'zstandard>=0.19.0 (extra: compression)',
+    'dataframe-api-compat>=0.1.7 (extra: consortium-standard)',
+    'adbc-driver-postgresql>=0.8.0 (extra: all)',
+    'adbc-driver-sqlite>=0.8.0 (extra: all)',
+    'beautifulsoup4>=4.11.2 (extra: all)',
+    'bottleneck>=1.3.6 (extra: all)',
+    'dataframe-api-compat>=0.1.7 (extra: all)',
+    'fastparquet>=2022.12.0 (extra: all)',
+    'fsspec>=2022.11.0 (extra: all)',
+    'gcsfs>=2022.11.0 (extra: all)',
+    'html5lib>=1.1 (extra: all)',
+    'hypothesis>=6.46.1 (extra: all)',
+    'jinja2>=3.1.2 (extra: all)',
+    'lxml>=4.9.2 (extra: all)',
+    'matplotlib>=3.6.3 (extra: all)',
+    'numba>=0.56.4 (extra: all)',
+    'numexpr>=2.8.4 (extra: all)',
+    'odfpy>=1.4.1 (extra: all)',
+    'openpyxl>=3.1.0 (extra: all)',
+    'pandas-gbq>=0.19.0 (extra: all)',
+    'psycopg2>=2.9.6 (extra: all)',
+    'pyarrow>=10.0.1 (extra: all)',
+    'pymysql>=1.0.2 (extra: all)',
+    'PyQt5>=5.15.9 (extra: all)',
+    'pyreadstat>=1.2.0 (extra: all)',
+    'pytest>=7.3.2 (extra: all)',
+    'pytest-xdist>=2.2.0 (extra: all)',
+    'python-calamine>=0.1.7 (extra: all)',
+    'pyxlsb>=1.0.10 (extra: all)',
+    'qtpy>=2.3.0 (extra: all)',
+    'scipy>=1.10.0 (extra: all)',
+    's3fs>=2022.11.0 (extra: all)',
+    'SQLAlchemy>=2.0.0 (extra: all)',
+    'tables>=3.8.0 (extra: all)',
+    'tabulate>=0.9.0 (extra: all)',
+    'xarray>=2022.12.0 (extra: all)',
+    'xlrd>=2.0.1 (extra: all)',
+    'xlsxwriter>=3.0.5 (extra: all)',
+    'zstandard>=0.19.0 (extra: all)'], "The Pandas Development Team", "1"])
 ]
 
 
 @pytest.fixture(params=values, ids=[str(x[0]) for x in values])
 def file_obj(request):
     return File(request.param[0])
-
-def test_whl_extraction(file_obj):
-    # Ensure that extraction does not raise any errors
-    file_obj.processor.process()
-
-    # Check that metadata fields are correctly extracted
-    metadata = file_obj.metadata
-    assert metadata["package_name"] is not None
-    assert metadata["version"] is not None
-    assert metadata["python_compatibility"] is not None
-    assert metadata["non_optional_dependencies"] is not None
-    assert metadata["optional_dependencies"] is not None
-    assert metadata["build_tag"] is not None
-    assert metadata["author"] is not None
-    assert metadata["platform_compatibility"] is not None
 
 def test_whl_save(file_obj):
     with tempfile.TemporaryDirectory() as temp_dir:
@@ -61,7 +121,7 @@ def test_whl_save(file_obj):
                 saved_whl.namelist())  # Check contents are still the same
 
 @pytest.mark.parametrize(variable_names, values)
-def test_whl_metadata(path, package_name, version, python_compatibility, platform_compatibility, non_optional_dependencies, optional_dependencies, build_tag, author):
+def test_whl_metadata(path, package_name, version, python_compatibility, platform_compatibility, non_optional_dependencies, optional_dependencies, author, build_tag):
     file_obj = File(path)
     metadata = file_obj.metadata
 
@@ -71,12 +131,13 @@ def test_whl_metadata(path, package_name, version, python_compatibility, platfor
     assert metadata['platform_compatibility'] == platform_compatibility
     assert metadata['non_optional_dependencies'] == non_optional_dependencies
     assert metadata['optional_dependencies'] == optional_dependencies
-    assert metadata['build_tag'] == build_tag
     assert metadata['author'] == author
+    assert metadata['build_tag'] == build_tag
+
 
 @pytest.mark.parametrize(variable_names, values)
-def test_save_whl_metadata(copy_file, package_name, version, python_compatibility, platform_compatibility, non_optional_dependencies, optional_dependencies, build_tag, author):
-    test_whl_metadata(copy_file, package_name, version, python_compatibility, platform_compatibility, non_optional_dependencies, optional_dependencies, build_tag, author)
+def test_save_whl_metadata(copy_file, package_name, version, python_compatibility, platform_compatibility, non_optional_dependencies, optional_dependencies, author, build_tag):
+    test_whl_metadata(copy_file, package_name, version, python_compatibility, platform_compatibility, non_optional_dependencies, optional_dependencies, author, build_tag)
 
 @pytest.mark.parametrize("path", map(lambda x: x[0], values))
 def test_whl_invalid_save_location(path):
