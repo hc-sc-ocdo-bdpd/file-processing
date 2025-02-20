f18c8f63cd108aea9acd00c4b8240078a9e10324
Brennan Cruse <brennan.cruse@hc-sc.gc.ca>
2025-02-20 13:09:41 -0500

    Update test_msg_processor.py

--- tests/unit/test_msg_processor.py
+++ tests/unit/test_msg_processor.py
@@ -14,7 +14,6 @@ values = [
     (test_files_path / 'Health Canada Overview from Wikipedia.msg', 13876, 'Health Canada Overview from Wikipedia', 'Tue, 31 Oct 2023 14:54:03 -0400', '"Prazuch, Karolina (HC/SC)" <karolina.prazuch@hc-sc.gc.ca>')
 ]
 
-
 @pytest.mark.parametrize(variable_names, values)
 def test_msg_metadata(path, text_length, subject, date, sender):
     file_obj = File(path)
@@ -33,13 +32,12 @@ def test_msg_metadata(path, text_length, subject, date, sender):
     assert file_obj.metadata['subject'] == subject
     assert processed_date == true_date
     assert file_obj.metadata['sender'] == sender
-
+    assert 'thread' in file_obj.metadata  # Ensure thread extraction works
+    assert isinstance(file_obj.metadata['thread'], dict)  # Ensure it's properly structured
 
 @pytest.mark.parametrize(variable_names, values)
 def test_save_msg_metadata(copy_file, text_length, subject, date, sender):
     test_msg_metadata(copy_file, text_length, subject, date, sender)
-
-
 
 @pytest.mark.parametrize("path", map(lambda x: x[0], values))
 def test_msg_invalid_save_location(path):
@@ -47,7 +45,6 @@ def test_msg_invalid_save_location(path):
     invalid_save_path = '/non_existent_folder/' + os.path.basename(path)
     with pytest.raises(FileProcessingFailedError):
         msg_file.processor.save(invalid_save_path)
-
 
 @pytest.mark.parametrize("path", map(lambda x: x[0], values))
 def test_not_opening_file(path):
